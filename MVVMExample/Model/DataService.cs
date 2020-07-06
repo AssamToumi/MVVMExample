d74a709b33f4091bb215244b98c1ee52fe8fb842
Assam Toumi <assamtomi@outlook.com>
2020-07-06 20:35:31 +0100

    done!

---
+++ MVVMExample/Model/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMExample.Model
+{
+    public class DataService
+    {
+        public string CustomerName { get; set; }
+        public string Speciality { get; set; }
+
+        public ObservableCollection<DataService> ListCustomers
+        {
+            get
+            {
+                return new ObservableCollection<DataService>()
+                {
+                new DataService() { CustomerName = "Assam Toumi", Speciality = ".net" },
+                new DataService() { CustomerName = "AssamToumi", Speciality = ".nett" },
+                new DataService() { CustomerName = "Assam Toumi", Speciality = ".net" },
+                new DataService() { CustomerName = "Assam Toumi", Speciality = ".net" },
+                new DataService() { CustomerName = "Assam Toumi", Speciality = ".net" }
+                 };
+            }
+
+        }
+    }
+}
+
---
+++ MVVMExample/ViewModel/MainViewModel.cs
@@ -0,0 +1,81 @@
+﻿using GalaSoft.MvvmLight.Command;
+using MVVMExample.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+
+namespace MVVMExample.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private DataService model;
+        private ICommand command;
+        private string customerName;
+        private string speciality;
+
+        private ObservableCollection<DataService> customers;
+        public string CustomerName
+        {
+            get { return customerName; }
+            set { customerName = value; OnPropertyChanged("CustomerName"); }
+        }
+        private string Speciality
+        {
+            get { return speciality; }
+            set { speciality = value; OnPropertyChanged("Speciality"); }
+        }
+        public ObservableCollection<DataService> Customers
+        {
+            get { return customers; }
+            private set
+            {
+                customers = value;
+                OnPropertyChanged("Customers");
+            }
+        }
+        public MainViewModel()
+        {
+            model = new DataService();
+        }
+
+        public ICommand ShowCommand
+        {
+            get
+            {
+                if (command == null)
+                {
+                    return command;
+
+                }
+                return command;
+            }
+        }
+
+        public bool CanDoCommand()
+        {
+            return model != null ? true : false;
+        }
+        public void DoCommand()
+        {
+            Customers = model.ListCustomers;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                this.PropertyChanged(this, e);
+            }
+        }
+    }
+}
